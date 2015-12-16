480af00aba542ad982f1692d68bef0e040d4d400
kavya-a <atmakurikavya@gmail.com>
2015-12-16 15:22:33 +0530

    slider-js

--- js/init.js
+++ js/init.js
@@ -3,6 +3,9 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $(document).ready(function(){
+      $('.slider').slider({full_width: false});
+    });
     //$('.collapsible').collapsible({accordion:false});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
