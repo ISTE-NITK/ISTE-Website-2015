8e8c1c96077455d855b1597d87c0f57d2fda58cf
kavya-a <atmakurikavya@gmail.com>
2015-12-17 19:54:06 +0530

    minor changes

--- js/init.js
+++ js/init.js
@@ -4,8 +4,18 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $(document).ready(function(){
-      $('.slider').slider({full_width: false});
+      $('.slider').slider({full_width: true});
     });
+      $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
     //$('.collapsible').collapsible({accordion:false});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
